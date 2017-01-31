a63df33e96082cf388fb55afe328a02ce6769797
Wojciech Dudek <wdudek82@gmail.com>
2017-02-01 00:21:12 +0100

    Tweaking html template

--- backend/urls.py
+++ backend/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+import apps
 
 
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + apps.urls.urlpatterns
 
 if settings.DEBUG:
     import debug_toolbar
